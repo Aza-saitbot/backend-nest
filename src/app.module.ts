a85a76cd465d86e51e953832306328db4c8f42bd
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2022-09-03 16:17:54 +0300

    feat/add swagger docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import {Module} from "@nestjs/common";
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
+import {User} from "./users/users.models";
 
 @Module({
 controllers:[],
@@ -17,7 +18,7 @@ controllers:[],
             username:process.env.POSTGRES_USERNAME,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [],
+            models: [User],
             autoLoadModels:true
         }),
         UsersModule,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
+import {UsersService} from "./users.service";
+import {CreateUserDto} from "./create-user.dto";
+import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {User} from "./users.models";
 
+@ApiTags('Пользователи')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private userService:UsersService) {
+    }
+
+    @ApiOperation({summary:'Создания пользователя'})
+    @ApiResponse({status:200,type:User})
+    @Post()
+    create(@Body() dto:CreateUserDto){
+        return this.userService.createUser(dto)
+    }
+
+    @ApiOperation({summary:'Получить всех пользователей'})
+    @ApiResponse({status:200,type:[User]})
+    @Get()
+    getAll(){
+        return this.userService.getUsers()
+    }
+}
