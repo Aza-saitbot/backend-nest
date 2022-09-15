2190f8223489655d01fe1fce64d577205989d6c6
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2022-09-16 00:10:04 +0300

    feat/auth м2

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,46 @@
+import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from "@nestjs/common";
+import {Observable} from "rxjs";
+import {JwtService} from "@nestjs/jwt";
+import {Reflector} from "@nestjs/core";
+import {ROLES_KEY} from "./roles-auth.decorator";
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate{
+    constructor(private jwtService:JwtService,
+                private reflector:Reflector
+                ) {
+    }
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const req=context.switchToHttp().getRequest()
+
+        try {
+            const requiredRoles=this.reflector.getAllAndOverride(ROLES_KEY,[
+                context.getHandler(),
+                context.getClass()
+            ])
+            if (!requiredRoles){
+                return true
+            }
+
+            const authHeader=req.headers.authorization
+            const bearer=authHeader.split(' ')[0]
+            const token=authHeader.split(' ')[1]
+
+            if (bearer !== 'Bearer'|| !token){
+                throw new UnauthorizedException({message:'Пользователь не авторизован'})
+            }
+
+            const user=this.jwtService.verify(token)
+            req.user=user
+            return user.roles.some(r=>requiredRoles.include(r.value))
+
+        }catch (e) {
+            console.log('Ошибка гуардах',e)
+            throw new UnauthorizedException({message:'Пользователь не авторизован'})
+        }
+    }
+
+    
+    
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import {SequelizeModule} from "@nestjs/sequelize";
@@ -6,13 +6,15 @@ import {User} from "./users.models";
 import {Role} from "../role/roles.models";
 import {UserRoles} from "../role/user-roles.model";
 import {RoleModule} from "../role/role.module";
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports:[
       SequelizeModule.forFeature([User,Role,UserRoles]),
-      RoleModule
+      RoleModule,
+      forwardRef(()=>AuthModule)
   ],
     exports:[UsersService]
 })
